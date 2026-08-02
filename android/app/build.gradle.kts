abd1f8972d513954f4aca0a892b45fb797e6200b
Adith .v.s <websinaro@gmail.com>
2026-08-02 11:15:17 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
 
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
-    implementation("com.google.android.material:material:<version>")
+    implementation("com.google.android.material:material:1.11.0")
 }
 
 flutter {
